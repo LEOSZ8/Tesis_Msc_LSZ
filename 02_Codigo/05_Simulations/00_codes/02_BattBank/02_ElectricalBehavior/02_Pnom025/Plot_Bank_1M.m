6fa504330c857385d245146cf1b1838a326244d5
LSZ_AC3E <leonardo.solis@alumnos.usm.cl>
2026-01-20 18:16:26 -0300

    :white_circle: Dev v5.5: Simulations (Appendix)

---
+++ 02_Codigo/05_Simulations/00_codes/02_BattBank/02_ElectricalBehavior/02_Pnom025/Plot_Bank_1M.m
@@ -0,0 +1,42 @@
+soc = 100.*soc;
+%%
+t = t./3600;
+%%
+A = tiledlayout(4,1,'TileSpacing','Compact');
+
+nexttile
+plot(t,soc,'LineWidth',1.5,'Color',"#00994D")
+ylim([40 110])
+yticks(50:25:100)
+%xlim([0 2])
+grid on
+ylabel('[%]','FontSize',16,'FontName','Times New Roman')
+legend('SoC(t)','Location','southwest','Orientation','horizontal','FontName','Times New Roman','FontSize',12)
+
+nexttile
+plot(t,Vbat,'LineWidth',1.5,'Color',[0.42 0.68 0.87])
+ylim([760 820])
+yticks(760:20:820)
+%xlim([0 2])
+grid on
+ylabel('[V]','FontSize',16,'FontName','Times New Roman')
+legend('{V}_{batt}(t)','Location','northeast','Orientation','horizontal','FontName','Times New Roman','FontSize',12)
+
+nexttile
+plot(t,Ibat,'LineWidth',1.5,'Color',[0.51 0.87 0.46])
+ylim([0 1300])
+yticks(0:300:1300)
+%xlim([0 2])
+grid on
+ylabel('[A]','FontSize',16,'FontName','Times New Roman')
+legend('{I}_{batt}(t)','Location','northwest','Orientation','horizontal','FontName','Times New Roman','FontSize',12)
+
+nexttile
+plot(t,Pload,'LineWidth',1.5,'Color',[0.60,0.62,0.58])
+ylim([0 1200])
+yticks(0:300:1200)
+%xlim([0 2])
+grid on
+ylabel('[kW]','FontSize',16,'FontName','Times New Roman')
+xlabel('Time [h]','FontSize',16,'FontName','Times New Roman')
+legend('{P}_{load}(t)','Location','northwest','Orientation','horizontal','FontName','Times New Roman','FontSize',12)
---
+++ 02_Codigo/05_Simulations/00_codes/03_BoostPowerConverter/01_Pnom015/Plot_TLBBC_015.m
@@ -0,0 +1,40 @@
+A = tiledlayout(4,1,'TileSpacing','Compact');
+
+nexttile
+plot(t,IL,'LineWidth',1.5,'Color',[0.51 0.87 0.46])
+ylim([-60 120])
+yticks(-80:40:120)
+xlim([0 2])
+grid on
+ylabel('[A]','FontSize',16,'FontName','Times New Roman')
+legend('{I}_{L}(t)','Location','northwest','Orientation','horizontal','FontName','Times New Roman','FontSize',12)
+
+nexttile
+plot(t,Vc1,'LineWidth',1.5,'Color',[0.42 0.68 0.87])
+ylim([0 600])
+yticks(100:200:600)
+xlim([0 2])
+grid on
+ylabel('[V]','FontSize',16,'FontName','Times New Roman')
+legend('{V}_{c1}(t), {V}_{c2}(t)','Location','southeast','Orientation','horizontal','FontName','Times New Roman','FontSize',12)
+
+nexttile
+plot(t,Vo_ref,':','LineWidth',1.5)
+hold on
+plot(t,Vo,'LineWidth',1.5,'Color',[0.42 0.68 0.87])
+ylim([0 1200])
+yticks(200:400:1200)
+xlim([0 2])
+grid on
+ylabel('[V]','FontSize',16,'FontName','Times New Roman')
+legend('{V}_{ref}(t)','{V}_{o}(t)','Location','southeast','Orientation','horizontal','FontName','Times New Roman','FontSize',12)
+
+nexttile
+plot(t,Pout,'LineWidth',1.5,'Color',"#00994D")
+ylim([0 40])
+yticks(0:10:40)
+xlim([0 2])
+grid on
+ylabel('[kW]','FontSize',16,'FontName','Times New Roman')
+xlabel('Time [s]','FontSize',16,'FontName','Times New Roman')
+legend('{P}_{out}(t)','Location','northwest','Orientation','horizontal','FontName','Times New Roman','FontSize',12)
