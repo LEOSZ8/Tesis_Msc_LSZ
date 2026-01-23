a85ac6f025609a79241fd0cb5de5f248c86eeec6
LSZ_Home <leonardo.solis@alumnos.usm.cl>
2026-01-23 08:04:25 -0300

    :white_circle: Dev v5.10: Correcciones de imágenes Marcelo Perez

--- 02_Codigo/03_HybridTugboat/01_codes/01_Pnom015/09_Comparation_1M_015.m
+++ 02_Codigo/03_HybridTugboat/01_codes/01_Pnom015/09_Comparation_1M_015.m
@@ -15,13 +15,15 @@ title("Conventional Tugboat : \newline   5,366 L (14.3 t CO_2)\newline  ","FontN
 clear
 clc
 % ht: hybrid tugboat
-ht = [0.17,0.19,0.09];
-labels2 = {'\bf 2000 kW - 3650 kW\newline             (38%)','\bf 1000 kW - 2000 kW\newline              (42%)','\bf 0 kW - 1000 kW\newline              (20%)'};
-p = pie(ht,labels2)
+ht = [0.17,0.19,0.09,0.55];
+labels2 = {'\bf 2000 kW - 3650 kW\newline             (38%)','\bf 1000 kW - 2000 kW\newline              (42%)','\bf 0 kW - 1000 kW\newline              (20%)','\bf BESS\newline (55%)'};
+explode = [0,0,0,1];
+p = pie(ht,explode,labels2)
 set(findobj(p,'type','text'),'FontName','times','FontSize',12)  % THIS LINE ADDED
 p(1).FaceColor=[0.60,0.62,0.58];
 p(3).FaceColor=[0.42 0.68 0.87];
 p(5).FaceColor=[0.42 0.68 0.87];
+p(7).FaceColor=[0.51 0.87 0.46];
 title("Hybrid Tugboat :\newline2,406 L (6.4 t CO_2)\newline  ","FontName",'Times New Roman','FontSize',18)
 
 
